31d3e3f8eda2865616feb4bf913bc414221697c3
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-17 08:41:21 +0100

    Fixed exceptions hierarchy tests

--- Doctrineum/Tests/SelfRegisteringType/SelfRegisteringTypeExceptionsHierarchyTest.php
+++ Doctrineum/Tests/SelfRegisteringType/SelfRegisteringTypeExceptionsHierarchyTest.php
@@ -1,15 +1,21 @@
 <?php
 namespace Doctrineum\Tests\SelfRegisteringType;
 
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
-class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
+class SelfRegisteringTypeExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return $this->getRootNamespace();
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
